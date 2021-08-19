c8c39702ea4d0dcdefddef9f4ede340c4f7c301b
manishas97 <msethia20@gmail.com>
2021-08-19 20:01:56 +0200

    Built Version

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-import streamlit as st
 from multiapp import MultiApp
-from apps import home, model_perspective, model, interactive_guide, heatmaps, pipeline, NLP_pipeline, create_your_pipeline # import your app modules here
+from apps import interactive_guide, home
+import streamlit as st
 
 app = MultiApp()
-
 # Add all your application here
-app.add_app("Home", home.app)
-app.add_app("Explore fairness in FinTech", pipeline.app)
-app.add_app("A helpful Guide", interactive_guide.app)
-app.add_app("Social Implications", heatmaps.app)
-app.add_app("Record viewpoints", model_perspective.app)
-app.add_app("Technical Detection NLP", NLP_pipeline.app)
-app.add_app("Create your pipeline", create_your_pipeline.app)
 
+app.add_app("Home", home.app)
+#app.add_app("General Awareness", awareness)
+#app.add_app("Dilemmas", interactive_guide.app)
+#app.add_app("Model Information", pipeline.app)
+#app.add_app("Perspectives", model_perspective.app)
+#app.add_app("Explore fairness", model_owner_mode.app)
+#app.add_app("Explore technical mitigation methods for fairness", model_developer.app)
+#app.add_app("Interface", interface.app)
 # The main app
 app.run()
